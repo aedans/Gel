1cf2b600df9bcae146c7f7b4171b8777d18b1c7b
Aedan Smith <aedan.smith@gmail.com>
2016-10-04 17:25:45 -0700

    Fixed installer.

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 mkdir C:\gel
-xcopy gel C:\gel\gel /i
-xcopy java C:\gel\java /i
-xcopy jar C:\gel\jar /i
+xcopy gel C:\gel\gel /i /s
+xcopy java C:\gel\java /i /s
+xcopy jar C:\gel\jar /i /s
 copy gel.bat C:\gel\gel.bat
 copy javagel.bat C:\gel\javagel.bat
