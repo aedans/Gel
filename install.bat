dfcde1499a5052cb334004cd24a4dfa8722fb207
Aedan Smith <aedan.smith@gmail.com>
2016-10-04 18:39:19 -0700

    Added c config.

--- install.bat
+++ install.bat
@@ -1,6 +1,8 @@
 mkdir C:\gel
 xcopy gel C:\gel\gel /i /s
 xcopy java C:\gel\java /i /s
+xcopy c C:\gel\c /i /s
 xcopy jar C:\gel\jar /i /s
 copy gel.bat C:\gel\gel.bat
 copy javagel.bat C:\gel\javagel.bat
+copy cgel.bat C:\gel\cgel.bat
